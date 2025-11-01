582fc6eea181270de313cb5a4ab9fe40787547aa
pragyasharma-cpu <pragya.sharma@lmsathena.com>
2025-11-01 22:02:38 +0530

    project

---
+++ React_Learning/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <div id='Parent'> 
+        <h1 id='Child1'>Hello Sister</h1>
+        <h3 id='Child2'>I am looking at your lies from far behind.</h3>
+      </div>
+  )
+}
+
+export default App
